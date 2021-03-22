30c19e4cba8eeb32ec3890ac3873b33546833997
Bostjan <bostjanzup@gmail.com>
2021-03-22 15:56:29 +0100

    inital commit

---
+++ src/Components/GeneralInfo.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class GeneralInfo extends Component {
+
+    render() {
+        return (
+            <h2>Hello from general Info</h2>
+        );
+    }
+}
+
+export default GeneralInfo;
